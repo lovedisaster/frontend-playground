399b3e9b326bf7ac3b261114fa7b4cff6daa8e1a
Nathan Zhang <nathan.zhang@rea-group.com>
2023-05-12 14:18:42 +1000

    Frontend playground with clock

---
+++ src/index.js
@@ -0,0 +1,19 @@
+setInterval(function() {
+const date = new Date();
+const hour = date.getHours() % 12;
+const minute = date.getMinutes();
+const second = date.getSeconds();
+
+const hourAng = hour*30 + (minute/60) * 30 + (second/3600)*30;
+const minuteAng = minute * 6 + (second/60)*6;
+const secondAng = second*6;
+
+const secondElement = document.querySelector(".second");
+const minuteElement = document.querySelector(".minute");
+const hourElement = document.querySelector(".hour");
+
+secondElement.style.transform = `translateY(-50%) rotate(${secondAng}deg)`
+minuteElement.style.transform = `translateY(-50%) rotate(${minuteAng}deg)`
+hourElement.style.transform = `translateY(-50%) rotate(${hourAng}deg)`
+}, 1000)
+
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const { watchFile } = require("fs");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: "./src/index.js",
+    output: {
+        filename: 'index-bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    devServer: {
+        port: 3000,
+        historyApiFallback: {
+          index: 'index.html'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|.jsx)$/, 
+                exclude: /node_modules/, 
+                use: {loader: 'babel-loader'}
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Frontend playground',
+        favicon: path.resolve('./public/favicon.ico'),
+        template: path.resolve('./public/index.html'),
+        filename: 'index.html',
+      })]
+};
